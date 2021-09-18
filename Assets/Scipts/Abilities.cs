f2feaa6ecd852a2a50c8e4dfd15fd42c4a93e3c1
DooDooWeeWoo <smithb2@stu.otc.school.nz>
2021-09-18 12:16:55 +1200

    Added Fonts and changing Prestige

--- Assets/Scipts/Abilities.cs
+++ Assets/Scipts/Abilities.cs
@@ -9,6 +9,7 @@ public class Abilities : MonoBehaviour
     public GameObject StrengthText;
     public GameObject PsychicText;
     public GameObject EnduranceText;
+    public GameObject TotalPowerText;
 
     public static int AgilityMultiplier = 1;
     public static int StrengthMultiplier = 1;
@@ -19,15 +20,18 @@ public class Abilities : MonoBehaviour
     public static int Agility = 1;
     public static int Endurance = 1;
     public static int Psychic = 1;
-
+    public static int TotalPower;
+    
     public static int PrestigeMultiplier = 1;
 
     void Update()
     {
+        TotalPower = (Agility+Strength+Endurance+Psychic);
         PlayerController.walkSpeed = Agility;
         AgilityText.GetComponent<Text>().text = "Agility: " + Agility;
         StrengthText.GetComponent<Text>().text = "Strength: " + Strength;
         PsychicText.GetComponent<Text>().text = "Psychic: " + Psychic;
         EnduranceText.GetComponent<Text>().text = "Endurance: " + Endurance;
+        TotalPowerText.GetComponent<Text>().text = "Total Power: " + TotalPower;
     }
 }
--- Assets/Scipts/Prestige.cs
+++ Assets/Scipts/Prestige.cs
@@ -7,12 +7,19 @@ public class Prestige : MonoBehaviour
 {
 
     public GameObject PrestigeText;
+    int TotalPowerRequirement;
 
     public void IncreasePrestigeMulti(){
 
-        Abilities.PrestigeMultiplier *= 2;
-        Tokens.tokenMultiplier *= 2;
-        PrestigeText.GetComponent<Text>().text = "Reputation Multiplier: x" + Abilities.PrestigeMultiplier;
-
+        if (Abilities.TotalPower>TotalPowerRequirement){
+            Abilities.Psychic = 0;
+            Abilities.Endurance = 0;
+            Abilities.Agility = 0;
+            Abilities.Strength = 0;
+            Abilities.PrestigeMultiplier *= 2;
+            Tokens.tokenMultiplier *= 2;
+            PrestigeText.GetComponent<Text>().text = "Reputation Multiplier: x" + Abilities.PrestigeMultiplier;
+            PrestigeText.GetComponent<Text>().text = "Required Total Power: " + TotalPowerRequirement;
+        }
     }
 }
