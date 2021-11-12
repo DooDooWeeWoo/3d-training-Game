0ec9103d200f794293b01d1183035f7e31016f3c
smithb2 <smithb2@stu.otc.school.nz>
2021-11-12 14:40:22 +1300

    Commented StatMultiIncrease

--- Assets/Scripts/StatMultiIncrease.cs
+++ Assets/Scripts/StatMultiIncrease.cs
@@ -5,10 +5,11 @@ using UnityEngine.UI;
 
 public class StatMultiIncrease : MonoBehaviour
 {
-    public GameObject StatsObject;
+    public GameObject StatsObject; //creating a gameobject which the Stats script is connected to
     [HideInInspector]
-    public Stats stat;
+    public Stats stat; //creating a referenceable script
 
+    //reating a bunch of prices which will go up over time
     public int PriceE = 100;
     public int PriceS = 100;
     public int PriceP = 100;
@@ -18,27 +19,24 @@ public class StatMultiIncrease : MonoBehaviour
     public GameObject PriceS_txt;
     public GameObject PriceP_txt; 
 
-    void Start()
+
+    void Start() //Upon Start
     {
-        stat = StatsObject.GetComponent<Stats>();
-        PriceE_txt.GetComponent<Text>().text = "Endurance: x" + stat.EnduranceMulti + " Price: " + PriceE;
-        PriceS_txt.GetComponent<Text>().text = "Strength: x" + stat.StrengthMulti + " Price: " + PriceS;
-        PriceP_txt.GetComponent<Text>().text = "Psychic: x" + stat.PsychicMulti + " Price: " + PriceP;
-        PriceA_txt.GetComponent<Text>().text = "Agility: x" + stat.AgilityMulti + " Price: " + PriceA;
-    }
-
-    void Update(){
-
+        stat = StatsObject.GetComponent<Stats>(); //get the Stats script and allow for writing to it.
+        PriceE_txt.GetComponent<Text>().text = "Endurance: x" + stat.EnduranceMulti + " Price: " + PriceE; //set the text in the inventory to show the multiplier price
+        PriceS_txt.GetComponent<Text>().text = "Strength: x" + stat.StrengthMulti + " Price: " + PriceS;//
+        PriceP_txt.GetComponent<Text>().text = "Psychic: x" + stat.PsychicMulti + " Price: " + PriceP;//
+        PriceA_txt.GetComponent<Text>().text = "Agility: x" + stat.AgilityMulti + " Price: " + PriceA;//
     }
 
     public void EnduranceIncrease(){
-        if (stat.Tokens>=PriceE){
-            stat.Tokens -= PriceE;
-            PriceE *= 2;
-            stat.EnduranceMulti *= 2;
-            PriceE_txt.GetComponent<Text>().text = "Endurance: x" + stat.EnduranceMulti + " Price: " + PriceE;
+        if (stat.Tokens>=PriceE){ //Check if the total amount of tokens the player has is more than the price
+            stat.Tokens -= PriceE; //subtract the price from the total tokens
+            PriceE *= 2; //double the price
+            stat.EnduranceMulti *= 2; //double the multplier for the stat
+            PriceE_txt.GetComponent<Text>().text = "Endurance: x" + stat.EnduranceMulti + " Price: " + PriceE; //update the text to display new price
         }
-    }
+    } //repeat this process for all of the different stats
 
     public void StrengthIncrease(){
         if (stat.Tokens>=PriceS){
