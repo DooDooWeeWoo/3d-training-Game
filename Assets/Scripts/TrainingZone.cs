01f26883ad9b0557dab83188becac260ef50dc4e
smithb2 <smithb2@stu.otc.school.nz>
2021-11-12 14:54:45 +1300

    Commented TraingZone Script

--- Assets/Scripts/TrainingZone.cs
+++ Assets/Scripts/TrainingZone.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class TrainingZone : MonoBehaviour
 {
-    float timer;
-    public bool Psychic;
+    float timer; //create a float variable for the timer
+    //create a few true or false variables so that the developer can adjust what zone type they want to use in the unity editor
+    public bool Psychic; 
     public bool Strength;
     public bool Endurance;
     public bool Agility;
@@ -13,35 +14,33 @@ public class TrainingZone : MonoBehaviour
     public GameObject StatsObject;
     [HideInInspector]
     public Stats stat;
-    [SerializeField] int statRequirement;
+    [SerializeField] int statRequirement; //create a stat requirement so that the player has to have a certain level in a stat
     public int ZoneMulti;
     
 
     void Start()
     {
         stat = StatsObject.GetComponent<Stats>();
-        
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other) //upon entering the training zone
     {
-        Debug.Log("Entered Training Zone");
-        timer = 0;
+        Debug.Log("Entered Training Zone"); //type into the log
+        timer = 0; //set the timer to zero
     }
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other) //while staying in the training zone
     {
-
-        
-        if (Endurance){
-            if(stat.Endurance>=statRequirement){
-                timer += Time.deltaTime;
-                if(timer>1){
-                    timer = 0;
-                    stat.Endurance += (ZoneMulti*stat.EnduranceMulti*stat.PrestigeMulti);
+        //check what bool the developer set the training zone to
+        if (Endurance){ 
+            if(stat.Endurance>=statRequirement){ //check if the player has the minimum required stat to use the zone
+                timer += Time.deltaTime; //start the timer
+                if(timer>1){ //upon every second
+                    timer = 0; //set the timer to zero
+                    stat.Endurance += (ZoneMulti*stat.EnduranceMulti*stat.PrestigeMulti); //increase the players stat by the zone multiplier(5 for example) * the stat multiplier * the prestige multiplier
                 }
             }
-        }
+        } //repeat the process with the other stats
 
         if (Strength){
             if(stat.Strength>=statRequirement){
